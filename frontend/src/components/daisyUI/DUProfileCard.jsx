68797b59bb16631812c7fe7aa8c8e62e3ae830c1
Roger Andrés Venegas Opazo <127449516+rogandven@users.noreply.github.com>
2026-01-07 03:37:16 -0300

    Added banner

--- frontend/src/components/daisyUI/DUProfileCard.jsx
+++ frontend/src/components/daisyUI/DUProfileCard.jsx
@@ -1,17 +1,22 @@
 import profilePic from "@assets/profilePic.jpg";
+import college from "@assets/college.jpg";
 
 export const DUProfileCard = ({ user }) => {
   return (
     <div>
-        <div class="avatar avatar-online">
-            <div class="w-24 h-24 rounded-full">
+        <div className="h-32 rounded-2xl" style={{backgroundImage: "url(" + college + ")", backgroundSize: "cover"}}>
+        </div>
+        <div className="avatar avatar-online -mt-16">
+            <div className="w-24 h-24 rounded-full border-base-100 border-4">
                 <img src={profilePic} />
             </div>
         </div>
 
+
+
           <div className="flex-row flex align-middle items-center ">
             <h2 className="text-2xl font-bold">{user.username}</h2>
-            <div class="ml-2 mr-2 badge badge-secondary">
+            <div className="ml-2 mr-2 badge badge-secondary">
                 {String(user.role).toUpperCase()}
             </div>
             <div class="badge badge-primary">
