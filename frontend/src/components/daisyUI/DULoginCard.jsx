c2cee03df2ce6ef9ddabee9754ce5df0b18e2baf
Roger Andrés Venegas Opazo <127449516+rogandven@users.noreply.github.com>
2026-01-06 23:18:39 -0300

    css

--- frontend/src/components/daisyUI/DULoginCard.jsx
+++ frontend/src/components/daisyUI/DULoginCard.jsx
@@ -24,13 +24,13 @@ export const DULoginCard = ({onSubmit, loginError, setLoginError, className, ema
     const insideContent = (
         <div className={className}>
             <h2 className="card-title mb-3">Iniciar sesión</h2>
-            {DUTextInput("email", "nombre@email.com", "Correo", "mb-3", "email-field", email, handleEmailChange)}
-            {DUTextInput("password", "**********", "Clave", "mb-3", "password-field", password, handlePasswordChange)}
+            {DUTextInput("email", "nombre@email.com", "Correo", "mb-3 w-full", "email-field", email, handleEmailChange)}
+            {DUTextInput("password", "**********", "Clave", "mb-3 w-full", "password-field", password, handlePasswordChange)}
             {error && <div role="alert" className="alert alert-error alert-soft flex-row flex mb-3">
                 <VscError className="mr-0"></VscError>
                 <span>{error}</span>
             </div>}   
-            <button className="btn btn-primary" disabled={error && true} onClick={onSubmit}>Entrar</button>         
+            <button className="btn btn-primary w-full" disabled={error && true} onClick={onSubmit}>Entrar</button>         
         </div>
     );
 
