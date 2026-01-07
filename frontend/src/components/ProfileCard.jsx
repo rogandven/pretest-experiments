d489e05b2e3ff5df2239ca4ca69e363ab0d1c170
Roger Andrés Venegas Opazo <127449516+rogandven@users.noreply.github.com>
2026-01-07 03:11:51 -0300

    Updated profile card

--- frontend/src/components/ProfileCard.jsx
+++ frontend/src/components/ProfileCard.jsx
@@ -1,25 +1,10 @@
 import profilePic from "@assets/profilePic.jpg";
+import DUProfileCard from "./daisyUI/DUProfileCard";
 
 const ProfileCard = ({ user }) => {
   return (
     <div className="profile-card">
-      <h1 className="profile-header">Perfil de {user.username}</h1>
-      <div className="profile-content">
-        <div className="profile-image">
-          <img src={profilePic} alt={`${user.username}'s profile`} />
-        </div>
-        <div className="profile-info">
-          <p>
-            <strong>Nombre:</strong> {user.username}
-          </p>
-          <p>
-            <strong>Correo:</strong> {user.email}
-          </p>
-          <p>
-            <strong>Rol:</strong> {user.role}
-          </p>
-        </div>
-      </div>
+      {DUProfileCard({user})}
     </div>
   );
 };
---
+++ frontend/src/components/daisyUI/DUProfileCard.jsx
@@ -0,0 +1,26 @@
+import profilePic from "@assets/profilePic.jpg";
+
+export const DUProfileCard = ({ user }) => {
+  return (
+    <div>
+        <div class="avatar avatar-online">
+            <div class="w-24 h-24 rounded-full">
+                <img src={profilePic} />
+            </div>
+        </div>
+
+          <div className="flex-row flex align-middle items-center ">
+            <h2 className="text-2xl font-bold">{user.username}</h2>
+            <div class="ml-2 badge badge-secondary">
+                {String(user.role).toUpperCase()}
+            </div>
+          </div>
+
+          <p>
+            {"@" + String(user.email).split("@")[0]}
+          </p>
+    </div>
+  );
+};
+
+export default DUProfileCard;
